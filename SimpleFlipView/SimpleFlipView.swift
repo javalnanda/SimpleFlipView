58095b3709f147b2d6e8c7c71fe0ba8ce93757ca
Javal Nanda <javalnanda@gmail.com>
2017-05-05 01:33:24 +0530

    Add project to repo

---
+++ SimpleFlipView/SimpleFlipView.swift
@@ -0,0 +1,53 @@
+//
+//  SimpleFlipView.swift
+//  SimpleFlipView
+//
+//  Created by Javal Nanda on 5/5/17.
+//  Copyright © 2017 Javal Nanda. All rights reserved.
+//
+
+import UIKit
+
+class SimpleFlipView: UIView {
+
+    // Front View
+    var frontView: UIView = UIView() {
+        didSet {
+            self.addSubview(frontView)
+        }
+    }
+    
+    // Back View
+    var backView: UIView = UIView() {
+        didSet {
+            
+        }
+    }
+    
+    // Set animation duration for transition
+    var animationDuration: Double = 0.5
+    
+    // Set transition option
+    var transitionStyle: UIViewAnimationOptions = UIViewAnimationOptions.transitionFlipFromRight
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.clear
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        flipView()
+    }
+    
+    func flipView()
+    {
+        UIView.transition(from: self.frontView.isDescendant(of: self) ? self.frontView : self.backView, to: self.frontView.isDescendant(of: self) ? self.backView : self.frontView, duration: animationDuration, options: transitionStyle) { (completed) in
+        }
+    }
+
+}
---
+++ SimpleFlipView/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  SimpleFlipView
+//
+//  Created by Javal Nanda on 5/5/17.
+//  Copyright © 2017 Javal Nanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //SimpleFlipView, frontView and backView added via storyboard
+    @IBOutlet weak var sbFlipView: SimpleFlipView!
+    @IBOutlet weak var sbSecondFlipView: SimpleFlipView!
+    @IBOutlet var frontView: UIView!
+    @IBOutlet var backView: UIView!
+    @IBOutlet var secondFvFrontView: UIView!
+    @IBOutlet var secondFvBackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Adding SimpleFlipView programmatically
+        // Make sure frontView, backView and the flipView are of same dimensions
+        let viewOne = UIView.init(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 100)))
+        let viewTwo = UIView.init(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 100)))
+        
+        viewOne.backgroundColor = UIColor.yellow
+        viewTwo.backgroundColor = UIColor.blue
+        
+        let flipView = SimpleFlipView.init(frame: CGRect(origin: CGPoint(x: 130, y: 100), size: CGSize(width: 100, height: 100)))
+        flipView.frontView = viewOne
+        flipView.backView = viewTwo
+        flipView.transitionStyle = UIViewAnimationOptions.transitionCurlUp
+        self.view .addSubview(flipView)
+        
+        // Setting up SimpleFlipView added via Storyboard
+        sbFlipView.frontView = self.frontView
+        sbFlipView.backView = self.backView
+        sbFlipView.transitionStyle = UIViewAnimationOptions.transitionFlipFromLeft
+        
+        // Setting up second SimpleFlipView added via Storyboard
+        sbSecondFlipView.frontView = self.secondFvFrontView
+        sbSecondFlipView.backView = self.secondFvBackView
+        sbSecondFlipView.animationDuration = 1.0
+        sbSecondFlipView.transitionStyle = UIViewAnimationOptions.transitionCrossDissolve
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
